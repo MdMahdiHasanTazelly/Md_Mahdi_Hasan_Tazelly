6243b7edab8adb7e7cd8089a7d7bb362efebe40d
Mahdi Hasan Tazelly <mahdihasantazelly@gmail.com>
2024-11-09 01:28:21 +0600

    Built Skills section & updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -5,14 +5,18 @@ function About(){
             <p>
                 Well!  <br/>
                 I&#39;m a dedicated <span className="highlighted">MERN Stack Developer</span> with experience in building secure 
-                and scalable web applications. Currently, I&#39;m persuing my under-grad degree, doing major on Computer Science & Engineering .
-                Proficient in <span className="highlighted">MongoDB, Express, React &amp; Node.js.</span> 
-                Again, I&#39;m proficienct on using some JS libraries e.g: <span className="highlighted">Bootstrap, Tailwind, Material-UI, Redux-reducers. </span>
+                and scalable web applications. Proficient in <span className="highlighted">Express, React, MongoDB, MySQL &amp; Node.js.</span> 
+                &nbsp;Again, I&#39;m proficienct on using some JS libraries e.g: <span className="highlighted">Bootstrap, Tailwind, Material-UI, Redux-reducers. </span>
                 I&#39;ve worked on various MERN applications, focusing on creating efficient, user-friendly interfaces and 
-                robust backends. For, backend, I'm good to go with <span className="highlighted">RESTful APIs</span>. And, I love to do it! 
-                If you want to go through my works, take a look on projects section
+                robust backends. As, version controlling system, I use <span className="highlighted">Git & GitHub </span>. 
+                I do unit testing with <span className="highlighted">JEST</span>. For, backend, I'm good to go with&nbsp;
+                <span className="highlighted">RESTful APIs</span>. And, I love to do it! If you want to go through my works, take a look on projects section.
                 <br/><br/>
-                I&#39;m open to collaboration and new opportunities, so feel free to reach out!
+                Currently, I&#39;m pursuing my undergrad degree on Computer Science & Engineering .
+                In my academia, I've learned <span className="highlighted">C, C++, Java,</span> and built projects on that. Moreover, I've learned many topics of 
+                <span className="highlighted"> Opearting Systems</span>, and how OS manages storage.
+                <br/><br/>
+                I&#39;m <span className="highlighted">open to collaboration and new opportunities</span>, so feel free to reach out!
             </p>
         </div>
     );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,133 @@
+function Skills() {
+  return (
+    <>
+        <div className="skill-section">
+          <h2>Skills</h2>
+          <div className="skillsContainer">
+            <div className="skills">
+               <h3>Frontend</h3>
+              <ul className="skill-list">
+                <li>
+                  <img src="/images/react.svg" />
+                  &nbsp; React
+                </li>
+                <li>
+                  <img src="/images/html.svg" />
+                  &nbsp; HTML
+                </li>
+                <li>
+                  <img src="/images/css.svg" />
+                  &nbsp; CSS
+                </li>
+               
+                <li>
+                  <img src="/images/bootstrap.png" />&nbsp;
+                  Bootstrap
+                </li>
+                <li>
+                  <img src="/images/tailwind-css.svg" />&nbsp; 
+                  Tailwind
+                </li>
+              </ul>
+                </div>
+        
+                <div className="skills">
+                  <h3>Backend & Databases</h3>
+                  <ul className="skill-list">
+                    <li>
+                      <img src="/images/express.svg" />
+                      &nbsp; Express
+                    </li>
+                    <li>
+                      <img src="/images/nodejs.svg" />
+                      &nbsp; Node
+                    </li>
+                    <li>
+                      <img src="/images/mysql.svg" />
+                      &nbsp; MySQL
+                    </li>
+                    <li>
+                      <img src="/images/mongodb.svg" />
+                      &nbsp; MongoDB
+                    </li>
+                  </ul>
+                </div>
+        
+                <div className="skills">
+                  <h3>Languages</h3>
+                  <ul className="skill-list">
+                    <li>
+                      <img src="/images/java.svg" />&nbsp;
+                      Java
+                    </li>
+                    <li>
+                      <img src="/images/c++.svg" />&nbsp; 
+                      C++
+                    </li>
+                    <li>
+                      <img src="/images/c.svg" />&nbsp; 
+                      C
+                    </li>
+                    <li>
+                      <img src="/images/javascript.svg" />&nbsp; 
+                      JavaScript
+                    </li>
+                  </ul>
+                </div>
+        
+                <div className="skills">
+                  <h3>Tools & Others</h3>
+                  <ul className="skill-list">
+                    <li>
+                      <img src="/images/git.svg" />&nbsp; 
+                      Git
+                    </li>
+                    <li>
+                      <img src="/images/github.svg" />&nbsp; 
+                      GitHub
+                    </li>
+                    <li>
+                      <img src="/images/jest.svg" />&nbsp; 
+                      JEST
+                    </li>
+                    <li>
+                      <img src="/images/redux.svg" />&nbsp; 
+                      Redux-Reducers
+                    </li>
+                  </ul>
+                </div>
+           </div>
 
 
-function Skills(){
-    return(
-        <h1>Skills</h1>
-    );
+           <div className="slider-container">
+            <div className="slider-animation">
+                <img src="/images/bootstrap.png"/>
+                <img src="/images/c.svg"/>
+                <img src="/images/c++.svg"/>
+                <img src="/images/css.svg"/>
+                <img src="/images/html.svg"/>
+                <img src="/images/express.svg"/>
+                <img src="/images/git.svg"/>
+                <img src="/images/github.svg"/>
+                <img src="/images/java.svg"/>
+                <img src="/images/javascript.svg"/>
+                <img src="/images/jest.svg"/>
+                <img src="/images/mongodb.svg"/>
+                <img src="/images/mysql.svg"/>
+                <img src="/images/nodejs.svg"/>
+                <img src="/images/react.svg"/>
+                <img src="/images/redux.svg"/>
+                <img src="/images/tailwind-css.svg"/>
+                <img src="/images/mui.svg"/>
+            </div>
+            
+        </div>
+    
+        </div>
+
+
+    </>
+  );
 }
 
 export default Skills;
